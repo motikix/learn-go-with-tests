00213b83fee721b60547f842599f4aaf41b93b4a
syuni <motikix@gmail.com>
2020-02-03 23:22:43 +0900

    Failed tests

--- mocking/main.go
+++ mocking/main.go
@@ -23,8 +23,12 @@ func (d *DefaultSleeper) Sleep() {
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
+
 	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
